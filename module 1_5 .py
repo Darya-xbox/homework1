e29ec93fea00286b797331164e86d5b4e2b957fa
Darya-xbox <pristavckina@yandex.ru>
2024-08-05 18:00:21 +0300

    Update module 1_5 .py

--- module 1_5 .py
+++ module 1_5 .py
@@ -1,8 +1,7 @@
 immutable_var = 1, 2, 3, True, 'a'
 print(immutable_var)
 # immutable_var[0][0] = 4, кортежи неизменяемы, что означает, что вы не можете изменять, добавлять или удалять элементы после создания кортежа
-mutable_list = 1, 2, [44, 45], 'a'
+mutable_list = [1, 2, 44, 45, 'a']
 print(mutable_list)
-mutable_list [2][0] = 20
+mutable_list [2] = 20
 print(mutable_list)
-

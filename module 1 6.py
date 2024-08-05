e13dc4f6df8694822ed3c73082fe4b3104558520
Darya-xbox <pristavckina@yandex.ru>
2024-08-05 17:49:12 +0300

    Add files via upload

---
+++ module 1 6.py
@@ -0,0 +1,17 @@
+my_dict = {'Darya': 2003, 'Mother': 1970, 'Father': 1968}
+print(my_dict)
+print(my_dict['Darya'])
+my_dict.get('Alex')
+print(my_dict.get('Alex'))
+my_dict.update({'Sister': 1990,
+                'Brother': 1995})
+print(my_dict)
+a = my_dict.pop('Darya')
+print(a)
+print(my_dict)
+my_set = {1, 52.52,'Banana'}
+print(my_set)
+my_set.add((48.90,'milf'))
+print(my_set)
+print(my_set.discard(52.52))
+print(my_set)

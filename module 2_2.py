3ee1250314ae8456e0597fbbadc24a0952526747
Darya-xbox <pristavckina@yandex.ru>
2024-08-06 15:15:42 +0300

    Add files via upload

---
+++ module 2_2.py
@@ -0,0 +1,19 @@
+first = 11
+second = 28
+third = 112
+if first==second==third:
+    print(3)
+elif first==second!=third or second==third!=first or first==third!=second:
+    print(2)
+elif first!=second!=third:
+    print(0)
+
+first = 44
+second = 44
+third = 16
+if first==second==third:
+    print(3)
+elif first==second!=third or second==third!=first or first==third!=second:
+    print(2)
+elif first!=second!=third:
+    print(0)
